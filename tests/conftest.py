331e092c58a7baba1875df26971b1a4145fc8c31
Ralph Corey <ralph.corey.1@gmail.com>
2025-01-14 05:25:31 -0800

    Renamed files

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 @pytest.fixture()
 def calendar_details():
     
-    '''Provide a modified petl table abstraction in order to test the api functions'''
+    """Provide a modified petl table abstraction in order to test the api functions"""
 
     load_dotenv()
     calendar_csv_file = os.getenv('TEST_CSV')
@@ -15,7 +15,7 @@ def calendar_details():
 @pytest.fixture()
 def og_csv():
 
-    '''Provide an unmodified version of the csv file in order to test modification functions'''
+    """Provide an unmodified version of the csv file in order to test modification functions"""
 
     load_dotenv()
     og_csv_file = os.getenv('TEST_OG_CSV')
